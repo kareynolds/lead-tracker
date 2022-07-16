ca52be476cd52aed0977084ae043d358d5c64e6e
kareynolds <ka.reynolds@outlook.com>
2022-07-16 13:20:01 -0700

    add anchor tag to list items in js
    utilized string pass through with back ticks to pass in myLeads
    to anchor tags

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 
 inputBtn.addEventListener("click", function() {
+    
     myLeads.push(inputEl.value)
-    console.log(myLeads);
+    renderLeads();
+    inputEl.value = '';
+
 })
 
 
-let listItems = '';
 
-for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + " " + "</li>";
+function renderLeads() {
+
+    let listItems = '';
+    for (let i = 0; i < myLeads.length; i++) {
+      
+        
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'> 
+                ${myLeads[i]}
+             </a>
+        </li>
+        `
+
+    }
+
+    ulEl.innerHTML = listItems; 
+
 }
-//Alterantively we could do:
-//const li = document.createElement("li")
-//li.textContent = myLeads[i]
-//ulEl.append(li)
-// This seems a bit convoluted though
 
 
-ulEl.innerHTML = listItems; 
