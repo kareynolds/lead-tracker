60875a97bffa9700cc17efe5720aeab3d2850b22
kareynolds <ka.reynolds@outlook.com>
2022-07-20 20:49:59 -0700

    add logic to delete button
    create condition to check if data exists in localstorage array

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
-
-//localStorage.setItem("myLeads", "google.com");
-
-let name = localStorage.getItem("myLeads");
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn");
 
 
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+console.log(leadsFromLocalStorage)
+
+// Check if leadsFromLocalStorage is truthy
+// If so, set myLeads to its value and call renderLeads()
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+} else {
+    console.log("No local storage datq")
+}
 
 inputBtn.addEventListener("click", function() {
-    
     myLeads.push(inputEl.value)
-    renderLeads();
-    inputEl.value = '';
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
 
 })
 
-
+deleteBtn.addEventListener("dblclick", function(){
+    myLeads = [];
+    localStorage.clear();
+    renderLeads();
+    
+})
 
 function renderLeads() {
-
-    let listItems = '';
+    let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-        
         listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'> 
-                ${myLeads[i]}
-             </a>
-        </li>
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
         `
-
     }
-
-    ulEl.innerHTML = listItems; 
-
+    ulEl.innerHTML = listItems  
 }
-
-
